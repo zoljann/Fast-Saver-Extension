2b46597b79b1f44fa2cadea095221539736dbdce
Nedim Zolj <74002374+zoljann@users.noreply.github.com>
2021-08-10 14:21:59 +0200

    UpdateExtension

--- FastLinksSave/style.js
+++ FastLinksSave/style.js
@@ -1,18 +1,18 @@
 const spasiDugmeBtn = document.getElementById("spasiDugme");
 let obrisiDugmeBtn = document.getElementById("obrisiDugme");
+let websiteBtn = document.getElementById("websiteDugme");
 const ulazEl = document.getElementById("ulaz-el");
 let ulEl = document.getElementById("ulEl");
 let nizLinkova = [];
 
 const nizLocalStorage = JSON.parse(localStorage.getItem("nizLinkova"));
-
-if(nizLocalStorage){
-    nizLinkova=nizLocalStorage;
+if (nizLocalStorage) {
+    nizLinkova = nizLocalStorage;
     osvjeziLinkove(nizLinkova);
 }
 
 spasiDugmeBtn.addEventListener("click", function () {
-    if(!(ulazEl.value === "")){
+    if (!(ulazEl.value === "")) {
         nizLinkova.push(ulazEl.value)
         ulazEl.value = ""
         localStorage.setItem("nizLinkova", JSON.stringify(nizLinkova))
@@ -20,7 +20,18 @@ spasiDugmeBtn.addEventListener("click", function () {
     }
 })
 
-obrisiDugmeBtn.addEventListener("dblclick", function(){
+const tabs = [
+    { url: "https://google.com" }
+]
+websiteBtn.addEventListener("click", function () {
+   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs){})
+   chrome.tabs.query({active: true})
+    nizLinkova.push(tabs[0].url);
+    localStorage.setItem("nizLinkova", JSON.stringify(nizLinkova));
+    osvjeziLinkove(nizLinkova);
+})
+
+obrisiDugmeBtn.addEventListener("dblclick", function () {
     localStorage.clear();
     nizLinkova = [];
     osvjeziLinkove(nizLinkova);
